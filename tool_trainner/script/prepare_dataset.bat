a1f5a0338c961efcabeb8070dd6ebb407038f5d6
sharonkv48 <sharonfx057@gmail.com>
2024-10-31 18:12:39 +0530

    first commit

---
+++ tool_trainner/script/prepare_dataset.bat
@@ -0,0 +1,2 @@
+python data_loader/prepare_syn.py --images-dir-rgb /SSD1/Dataset_DRLISP/ --images-dir-raw /SSD1/Dataset_DRLISP_RAW/ --output-path /SSD1/Dataset_DRLISP_RAW/syn_dataset/  --patch-size 224 
+python data_loader/prepare_syn.py --images-dir-rgb /SSD1/Dataset_DRLISP/ --images-dir-raw /SSD1/Dataset_DRLISP_RAW/ --output-path /SSD1/Dataset_DRLISP_RAW/syn_dataset/  --patch-size 224  --test 
